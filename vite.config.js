efbd0ef0c723796365010a69737e6979cdb5291c
tatenda.tobaiwa <tatenda.tobaiwa@gmail.com>
2025-01-20 10:19:51 +0000

    push

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,13 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { resolve } from 'path';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 export default defineConfig({
+  base: '/', 
   plugins: [react()],
   resolve: {
     alias: {
-      '@': resolve(__dirname, './src'), // Alias '@' for the 'src' folder
+      '@': resolve(__dirname, './src'),
     },
   },
 });
